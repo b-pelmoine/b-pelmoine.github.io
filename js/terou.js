d1494cc0c6a3cf847de2164cd56253ba403a4d54
Tero <contact.tero@gmail.com>
2017-07-16 23:58:25 +0200

    -MOD : ready to be working on ! :dancer:

--- js/terou.js
+++ js/terou.js
@@ -1,5 +1,4 @@
 (function () {
-
     $('a[href*="#"]:not([href="#"])').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
@@ -12,5 +11,39 @@
             }
         }
     });
-
 }).call(this);
+
+$(function() {
+    Pace.on('done', function() {
+        setTimeout(function() {
+            $("#loader").fadeOut();
+        }, 500);
+        //nameChanger
+        var stateParams = 
+        {
+            States : ["Concept Artist","Bug Fixer","Shader Enthusiast", "Game Addict", "C++ Lover", "2D Artist", "Game Programmer"],
+            stateIndex : 0,
+            stateSelector : $("#container .headline .state"),
+            stateIn : "slideInUp",
+            stateOut : "slideOutUp"
+        }
+        setInterval(function(){rollNewState(stateParams)}, 2000);
+    });
+});
+
+
+function rollNewState(state)
+{
+    console.log("1");
+    if(state.stateIndex >= state.States.length)
+        state.stateIndex = 0;
+    var newState = state.States[state.stateIndex++];
+    state.stateSelector.toggleClass(state.stateIn);
+    state.stateSelector.toggleClass(state.stateOut);
+    setTimeout(function() {
+            state.stateSelector.toggleClass(state.stateOut);
+            state.stateSelector.text(" " + newState.toLowerCase());
+            state.stateSelector.toggleClass(state.stateIn);
+        }, 1000);
+}
+
